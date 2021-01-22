1ed5459a40a9ba7d88b97fb13bc8fef6b702fed4
jdhayhurst <hayhurst.jd@gmail.com>
2021-01-22 16:52:08 +0000

    singularity binding, adding ptrepack step

--- loading/consolidate_study_hdf.nf
+++ loading/consolidate_study_hdf.nf
@@ -1,7 +1,7 @@
 process consolidate_hdfs_by_chrom {
 
-  containerOptions "--bind $params.data_dir"
-  publishDir "$params.hdf5_chrom_dir", mode: 'copy'
+  containerOptions "--bind $params.meta_table --bind $params.hdf5_study_dir --bind $params.hdf5_chrom_dir"
+  publishDir "$params.hdf5_chrom_dir", mode: 'move'
 
   memory { 8.GB * task.attempt }
   maxRetries 3
@@ -14,10 +14,22 @@ process consolidate_hdfs_by_chrom {
   output:
   file "file_${chr}.${method}.h5" optional true into hdf5_chrom
 
+  script:
   """
   echo $chr;
   echo $method;
-  eqtl-consolidate -in_dir $params.hdf5_study_dir -out_file file_${chr}.${method}.h5 -meta  $params.meta_table -quant $method -chrom $chr
+  hdf="${chr}.${method}.h5"
+  out="file_${chr}.${method}.h5"
+  for f in $params.hdf5_study_dir/$chr/*+"$method".h5; do
+        echo \$f;
+        if [ -f \$f ]; then
+                eqtl-consolidate -in_file \$f -out_file \$hdf -meta $params.meta_table -quant $method -chrom $chr
+        fi
+  done
+  if [ -f \$hdf ]; then
+        eqtl-reindex -f \$hdf
+        ptrepack --chunkshape=auto --propindexes --complevel=9 --complib=blosc \$hdf \$out
+  fi
   """
 
 }
--- sumstats/utils/consolidate_hdf.py
+++ sumstats/utils/consolidate_hdf.py
@@ -52,10 +52,10 @@ def parse_meta(file, quant, chrom, meta):
     meta_df = pd.read_csv(meta, sep='\t')
     meta_for_file = meta_df[(meta_df['study'] == study) & (meta_df['qtl_group'] == qtl_group) & (meta_df['quant_method'] == quant)]
     if meta_for_file.empty:
-        print("filename {} not found in metadata".format(ss_file))
+        print("filename {} not found in metadata".format(file))
         sys.exit(1)
     elif len(meta_for_file) > 1:
-        print("filename {} found more than once in metadata".format(ss_file))
+        print("filename {} found more than once in metadata".format(file))
         sys.exit(1)
     else:
         tissue_ont = meta_for_file['tissue_ontology_id'].values[0]
@@ -64,38 +64,23 @@ def parse_meta(file, quant, chrom, meta):
 
 def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('-in_dir', help='The path to the hdf in dir e.g. study dir', required=False)
+    argparser.add_argument('-in_file', help='The path to the hdf in e.g. study hdf5', required=False)
     argparser.add_argument('-out_file', help='The path to the hdf out file e.g. chrom + quant file ', required=False)
     argparser.add_argument('-meta', help='The metadata tsv file', required=False)
     argparser.add_argument('-quant', help='The quant method', required=False)
     argparser.add_argument('-chrom', help='The chromosome', required=False)
 
-
-
-    #argparser.add_argument('-key', help='The hdf key/group', required=False)
-    #argparser.add_argument('-study', help='The study identifier', required=False)
-    #argparser.add_argument('-qtl_group', help='The qtl group e.g. "LCL"', required=False)
-    #argparser.add_argument('-tissue_ont', help='The tissue ontology term', required=False)
-
     args = argparser.parse_args()
 
-    in_dir = args.in_dir
+    in_file = args.in_file
     out_file = args.out_file
     meta = args.meta
     quant = args.quant
     chrom = args.chrom
-    #study = args.study
-    #key = args.key
-    #qtl_group = args.qtl_group
-    #tissue_ont = args.tissue_ont
 
     print(out_file)
-    matching_files = glob.glob(os.path.join(in_dir, chrom, "*+" + quant + ".h5"))
-    for file in matching_files:
-        study, key, qtl_group, tissue_ont = parse_meta(file, quant, chrom, meta)
-        append_to_file(file, out_file, study, key, qtl_group, tissue_ont)
-    indexer = ri.H5Indexer(out_file)
-    indexer.reindex_file()
+    study, key, qtl_group, tissue_ont = parse_meta(in_file, quant, chrom, meta)
+    append_to_file(in_file, out_file, study, key, qtl_group, tissue_ont)
 
 
 if __name__ == "__main__":
