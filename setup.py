e11d941b5c7b696ee3050a43678a3f9333d6f3db
jdhayhurst <hayhurst.jd@gmail.com>
2023-02-08 09:38:46 +0000

    adding minimal cli for hdf5 convert

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@ from distutils.core import setup
 setup(
     name='eqtl-sumstats',
     version='0.1.0',
-    packages=['sumstats.api_v1', 'sumstats.api_v1.utils', 'sumstats.api_v1.server', 'sumstats.api_v1.study', 'sumstats.api_v1.study.search', 'sumstats.api_v1.study.search.access', 'sumstats.api_v1.trait', 'sumstats.api_v1.trait.search',
-              'sumstats.api_v1.trait.search.access', 'sumstats.api_v1.chr', 'sumstats.api_v1.chr.search', 'sumstats.api_v1.chr.search.access', 'config', 'sumstats.dependencies.errors'],
+    packages=['sumstats.api_v2',
+              'sumstats.api_v2.cli',
+              'sumstats.api_v2.services',
+              'sumstats.api_v2.schemas',
+              'sumstats.api_v2.utils',
+              'config',
+              'sumstats.dependencies'],
     entry_points={
-        "console_scripts": ['eqtl-load = sumstats.api_v1.load:main',
-                            'eqtl-search = sumstats.api_v1.controller:main',
-                            'eqtl-explore = sumstats.api_v1.explorer:main',
-                            'eqtl-rebuild-snps = sumstats.api_v1.utils.vcf_to_sqlite:main',
-                            'eqtl-prep-file = sumstats.api_v1.utils.split_by_chrom:main',
-                            'eqtl-reindex = sumstats.api_v1.reindex:main',
-                            'eqtl-delete = sumstats.api_v1.deleter:main',
-                            'eqtl-consolidate = sumstats.api_v1.utils.consolidate_hdf:main']
+        "console_scripts": ['eqtl-load = sumstats.api_v2.cli.main:main']
     },
     url='https://github.com/EBISPOT/SumStats',
     license='',
