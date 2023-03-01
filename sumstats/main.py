5ab3566a3c9377792757b1a512609ac9439585d0
jdhayhurst <hayhurst.jd@gmail.com>
2023-03-01 13:55:28 +0000

    update docs

--- sumstats/main.py
+++ sumstats/main.py
@@ -13,9 +13,33 @@ logging.config.fileConfig("sumstats/log_conf.ini",
 logger = logging.getLogger(__name__)
 
 API_BASE = "/eqtl/api"
+DESCRIPTION = """
+<img src='https://raw.githubusercontent.com/eQTL-Catalogue/eQTL-Catalogue-website/gh-pages/static/eQTL_icon.png' alt="eqtl_icon" width="10%">
 
-description = """
-Placeholder
+Welcome to the [eQTL Catalogue](https://www.ebi.ac.uk/eqtl) RESTful API.
+
+This API is for facilitating  a filtered request of 
+eQTL association data.
+
+## API v2
+
+Each study in the catalogue is split by QTL context and these splits are
+assigned their own dataset IDs (QTD#). Datasets can be browsed at the `/datasets` 
+endpoint. 
+
+To retrieve the summary statistics for a dataset, use the 
+`/datasets/<DATASETID>/associations` endpoint and apply 
+any required filters. 
+
+## API v1
+
+This will be deprecated and is maintained only for existing integrations. 
+
+## All associations
+If you are interested in downloading 
+_all_ the association data for a dataset, the recommended 
+method would be via the FTP.
+
 """
 
 tags_metadata = [
@@ -32,7 +56,7 @@ tags_metadata = [
 
 app = FastAPI(title="eQTL Catalogue Summary Statistics API Documentation",
               openapi_tags=tags_metadata,
-              description=description,
+              description=DESCRIPTION,
               docs_url=f"{API_BASE}/docs",
               redoc_url=None,
               openapi_url=f"{API_BASE}/openapi.json")
