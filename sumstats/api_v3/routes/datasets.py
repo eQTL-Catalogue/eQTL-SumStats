e25e1a8d29e40feeea2f292355c2b4cc01e6abe7
Karatuğ Ozan Bircan <karatugo@ebi.ac.uk>
2025-04-07 17:25:01 +0100

    docs: Fix summary texts

--- sumstats/api_v3/routes/datasets.py
+++ sumstats/api_v3/routes/datasets.py
@@ -16,27 +16,29 @@ from sumstats.api_v3.models.schemas import (
 )
 from sumstats.config import API_BASE
 
-router = APIRouter(
-    prefix=f"{API_BASE}/v3/datasets", tags=["eQTL API v3 Datasets"]
-)
+router = APIRouter(prefix=f"{API_BASE}/v3/datasets", tags=["eQTL API v3"])
 
 
-@router.get("", response_model=List[DatasetModel])
+@router.get("", summary="Get Datasets", response_model=List[DatasetModel])
 async def get_datasets_route(
     client: AsyncIOMotorClient = Depends(get_mongo_client),
 ):
     """
-    Lists all unique dataset_ids with their latest study_id and status.
+    Lists all datasets.
     """
     return await list_datasets(client)
 
 
-@router.get("/{dataset_id}", response_model=DatasetModel)
+@router.get(
+    "/{dataset_id}",
+    summary="Get Dataset Metadata",
+    response_model=DatasetModel,
+)
 async def get_dataset_route(
     dataset_id: str, client: AsyncIOMotorClient = Depends(get_mongo_client)
 ):
     """
-    Retrieves the latest info for a given dataset_id.
+    Fetch single dataset details.
     """
     dataset = await get_single_dataset(client, dataset_id)
     if not dataset:
@@ -44,7 +46,11 @@ async def get_dataset_route(
     return dataset
 
 
-@router.get("/{dataset_id}/search", response_model=List[AssociationModel])
+@router.get(
+    "/{dataset_id}/search",
+    summary="Search Within A Dataset",
+    response_model=List[AssociationModel],
+)
 async def search_within_dataset_route(
     dataset_id: str,
     gene_id: Optional[str] = Query(None),
@@ -55,7 +61,7 @@ async def search_within_dataset_route(
     client: AsyncIOMotorClient = Depends(get_mongo_client),
 ):
     """
-    Search within a specific dataset by gene_id, rsid, or variant.
+    Search within a specific dataset by search filters.
     """
     filters = SearchFilters(
         gene_id=gene_id,
