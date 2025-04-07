9ffaddd225f396795802c8bf8a3fa78e7feaa7d2
Karatuğ Ozan Bircan <karatugo@ebi.ac.uk>
2025-04-07 13:26:10 +0100

    feat: Add v3 mongo client

---
+++ sumstats/api_v3/db/client.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from sumstats.api_v3.core.config import settings
+
+# Global variable to store the client instance
+_mongo_client: Optional[AsyncIOMotorClient] = None
+
+
+def get_mongo_client() -> AsyncIOMotorClient:
+    """
+    Get or create a MongoDB client using the singleton pattern.
+    This ensures we only create one client instance during the
+    application lifecycle.
+    """
+    global _mongo_client
+    if _mongo_client is None:
+        _mongo_client = AsyncIOMotorClient(settings.mongo_uri)
+    return _mongo_client
